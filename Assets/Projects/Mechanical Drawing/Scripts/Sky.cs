9784ef9cfe090ad04dd3d2f2e769f75bfce09476
MOTH_SANCTUARY\vixsa <vix.sak@gmail.com>
2026-02-16 20:32:28 -0500

    added a background

--- Assets/Projects/Mechanical Drawing/Scripts/Sky.cs
+++ Assets/Projects/Mechanical Drawing/Scripts/Sky.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class Sky : MonoBehaviour
 {
-
+    Vector2 bottomLeft;
+    Vector2 topRight;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,16 +12,20 @@ public class Sky : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
+        //get screen limits
+        bottomLeft = Camera.main.ScreenToWorldPoint(Vector2.zero);
+        topRight = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
 
-
-        //scale the sky colour based on the screen size
+        //scale the backroung(repurposed from first project) colour based on the screen size
         Vector2 newSize = transform.localScale;
 
         //make it dependent on the height and width
-        newSize.x = Screen.width;
-        newSize.y = Screen.height;
+        newSize.x = topRight.x - bottomLeft.x;
+        newSize.y = topRight.y - bottomLeft.y;
 
         //update scale
-        transform.localScale = newSize;
+        //divide by 4 to make it fit the screen better, it was too big before
+        //idk why it makes it huge but it does, so im just gonna divide it by 4 and call it a day
+        transform.localScale = newSize/4;
     }
 }
