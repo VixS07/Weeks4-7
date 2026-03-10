4ac56a67368762ba7cdf3998e6858df22d10df50
Vix <vix.sak@gmail.com>
2026-03-10 07:01:46 -0400

    watched debugging strategies

---
+++ Assets/Debugging/Strategies.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Strategies : MonoBehaviour
+{
+    public GameObject prefab;
+
+    void Start()
+    {
+        for (float i = 0; i < 10; i++)
+        {
+            //instantiate a prefab
+            //at a x position thats increasing by 1
+            //a y position thats increasing by 0.1
+            //and 0 in z, no rotation
+
+            float x = i;
+            Debug.Log(i + "/" + 10 + " = " + i/10);
+            float y = i/10;
+            Debug.Log("Y = " + y);
+
+            float z = 0;
+
+            Instantiate(prefab, new Vector3 (x,y,z), Quaternion.identity);
+            
+        }
+    }
+
+}
